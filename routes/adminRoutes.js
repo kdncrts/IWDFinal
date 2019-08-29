6498a7b56b27ce8ec9b524d554b8dda4e226f476
Kaden Curtis <kdncrts@gmail.com>
2019-08-29 02:27:23 -0600

    Minor admin route fixes

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -7,7 +7,7 @@ var router = express.Router();
 // These routes use the Promise Example I posted
 router.route("/users").get(
     function(req, res){
-        if(req.session.user.role == "admin") {
+        if(req.session.user != undefined && req.session.user.role == "admin") {
             const model = {
                 header: ModelUtils.buildHeader("/admin/users", req.session.user),
             };
@@ -20,7 +20,7 @@ router.route("/users").get(
             const model = {
                 header: ModelUtils.buildHeader("/", req.session.user)
             };
-            res.render("/", model)
+            res.render("index", model)
         }
     }
 );
@@ -33,7 +33,7 @@ router.route("/user/activate/:email").get(
 
 router.route("/user/suspend/:email").get(
     function(req, res){
-        req.query.email
+        console.log(req.query.email);
     }
 );
 
